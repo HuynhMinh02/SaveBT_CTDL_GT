8f458f40ed491a81103cab66b8fe8a7fd7b1367a
HuynhMinh02 <huynhuuminh908@gmail.com>
2021-11-01 08:54:46 +0700

    cap nhat tuan 09

---
+++ src/com/minhhh2004110025/tuan09/DemoSapXep.java
@@ -0,0 +1,57 @@
+public class DemoSapXep {
+    public void selectionSort(int arr[]) {
+        int index, i, j;
+ 
+        // lap qua ta ca cac so
+        for (i = 0; i < arr.length - 1; i++) {
+            // thiet lap phan tu hien tai la min
+            index = i;
+ 
+            // kiem tra phan tu hien tai co phai la nho nhat khong
+            for (j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
+                }
+            }
+ 
+            if (index != i) {
+                System.out.println(" ==> Trao doi phan tu: [" + arr[i] 
+                        + ", " + arr[index] + "]");
+                // Trao doi cac so
+                int temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+            }
+ 
+            System.out.println("Vong lap thu " + (i + 1));
+            xuat(arr);
+        }
+    }
+ 
+    public void xuat(int arr[]) {
+        int i;
+        System.out.print("[");
+ 
+        // Duyet qua tat ca phan tu
+        for (i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+ 
+        System.out.print("]\n");
+    }
+ 
+    public static void main(String[] args) {
+        // khoi tao mang arr
+        int arr[] = { 6, 7, 0, 2, 8, 1, 3, 9, 4, 5 };
+ 
+        DemoSapXep demoSapXep = new DemoSapXep();
+        System.out.println("Mang du lieu dau vao: ");
+        demoSapXep.xuat(arr);
+        System.out.println("-----------------------------");
+        demoSapXep.selectionSort(arr);
+        System.out.println("-----------------------------");
+        System.out.println("\nMang sau khi da sap xep: ");
+        demoSapXep.xuat(arr);
+    }
+}
+
---
+++ src/com/minhhh2004110025/tuan09/SapXepLuaChon.java
@@ -0,0 +1,40 @@
+public class SapXepLuaChon {
+    public void selectionSort(int arr[]) {
+        int index, i, j;
+ 
+        // lap qua ta ca cac so
+        for (i = 0; i < arr.length - 1; i++) {
+            // thiet lap phan tu hien tai la min
+            index = i;
+ 
+            // kiem tra phan tu hien tai co phai la nho nhat khong
+            for (j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+            xuat(arr);
+        }
+
+    }
+    public void xuat(int arr[]) {
+        System.out.println("Mang sau khi sap xep la:");
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        // khoi tao mang arr
+        int arr[] = { 6, 7, 0, 2, 8, 1, 3, 9, 4, 5 };
+ 
+        DemoSapXep demoSapXep = new DemoSapXep();
+
+        demoSapXep.selectionSort(arr);
+        demoSapXep.xuat(arr);
+    }
+}
+    
+
